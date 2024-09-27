1e31a406fc966e5d10b2fc530d4918e253f5fed0
stepaz-2022164 <stepaz-2022164@kinal.edu.gt>
2024-09-27 17:35:04 -0600

    Login funcional con diseño básico

--- InventarioTEC/src/app/app.module.ts
+++ InventarioTEC/src/app/app.module.ts
@@ -3,6 +3,11 @@ import { TokenInterceptor } from "./services/token.interceptor";
 import { JwtModule } from "@auth0/angular-jwt";
 import { NgModule } from "@angular/core";
 import { LoginComponent } from './components/login/login.component';
+import { AppComponent } from "./app.component";
+import { BrowserModule } from "@angular/platform-browser";
+import { FormsModule } from "@angular/forms";
+import { RouterModule } from "@angular/router";
+import { AppRoutingModule } from "./app-routing.module";
 
 export const getToken = () => {
   return localStorage.getItem('token');
@@ -10,10 +15,15 @@ export const getToken = () => {
 
 @NgModule({
   declarations: [
-    LoginComponent
+    LoginComponent,
+    AppComponent
   ],
   imports: [
     HttpClientModule,
+    BrowserModule,
+    FormsModule,
+    RouterModule,
+    AppRoutingModule,
     JwtModule.forRoot({
       config: {
         tokenGetter: getToken,
@@ -25,6 +35,6 @@ export const getToken = () => {
   providers: [
     { provide: HTTP_INTERCEPTORS, useClass: TokenInterceptor, multi: true }
   ],
-  bootstrap: [LoginComponent]
+  bootstrap: [AppComponent]
 })
 export class AppModule {}
